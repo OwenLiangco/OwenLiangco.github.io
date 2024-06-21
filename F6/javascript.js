c3b22da46e2b1eae025f10a3a66f9f9cc28af55b
Owen Liangco <owenliangco21@gmail.com>
2024-06-21 13:34:12 +0800

    FormativeSixSomewhatDone

--- F6/javascript.js
+++ F6/javascript.js
@@ -13,7 +13,7 @@ function handleSubmit(event) {
     event.preventDefault(); 
     validatePassword();
     if (confirm_password.validity.valid) {
-        window.location.href = "http://www.google.com"; 
+        window.location.href = "https://owenliangco.github.io/F6/login.html"; 
     }
 }
 
